51f51cdfb11bffba33407f94655539640d6c31dd
petruteo <escu.petru@gmail.com>
2018-05-23 19:56:09 +0300

    stuff

--- app.js
+++ app.js
@@ -3,45 +3,59 @@ const PATH = require('path');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
- 
+
 // Jquery extraction from page
 
 extractFromPage = (file) => {
-    const dom = new JSDOM(file);
+  const dom = new JSDOM(file);
 
-    function extractEmails (text)
-  {
-      return text//.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-  };
-  
-  function extractPattern (text)
-  {
-      return text.match(/([a-zA-Z0-9{}._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-  };
-  
-  var link = [];
-  if (!dom.window.document.querySelector(".company-intro-container")) {
-    var link = dom.window.document.querySelector(".company-intro-container").textContent;
+  function extractEmails(text) {
+    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
   };
 
-//   var patternExtract = dom.window.document.querySelector(".search-pattern");
-  var pattern = [];
-  if (!patternExtract) {
-     pattern = extractPattern(dom.window.document.querySelector(".search-pattern").textContent);
+  function extractPattern(text) {
+    return text.match(/([a-zA-Z0-9{}._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
   };
+
+  function extractDomain(text) {
+    return text.match(/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/);
+  };
+
+
+  var pattern = extractPattern(file);
+  var emails = extractEmails(file);
+  // //link extraction
+  var linkExtract = dom.window.document.querySelector(".company-intro-container");
+  if (linkExtract) {
+    // var link = extractDomain(linkExtract.textContent);
+    var link = linkExtract.getElementsByTagName('a');
+
+  } else {
+    link = [];
+  };
+
+  // // pattern extraction 
+  // var patternExtract = dom.window.document.querySelector(".search-pattern");
+  // var pattern = [];
+  // if (!patternExtract) {
+  //   pattern = extractPattern(dom.window.document.querySelector(".search-pattern").textContent);
+  // };
+
+  //results extraction
   //var result = extractEmails(dom.window.document.querySelector(".result").textContent);
-  console.log("------", pattern);
-  
-  
-//   var link = dom.window.document.querySelector(".company-intro-container").find('a').attr('href');
-//   var pattern = extractPattern(dom.window.document.querySelector(".search-pattern").text());
-//   var result = extractEmails(dom.window.document.querySelector(".result").text());
+  // console.log("------", pattern);
+  // console.log("******", emails);
 
-  var data = {link, pattern, result};
-      
-      return data;
-  }; 
-    
+
+  //   var link = dom.window.document.querySelector(".company-intro-container").find('a').attr('href');
+  //   var pattern = extractPattern(dom.window.document.querySelector(".search-pattern").text());
+  //   var result = extractEmails(dom.window.document.querySelector(".result").text());
+
+  var data = { link, pattern, emails };
+
+  return data;
+};
+
 
 // READ FILES
 
@@ -66,10 +80,15 @@ function readFiles(dirPath, processFile) {
 };
 
 readFiles('./pages/', (name, ext, path, file, data) => {
-    // console.log(`file name is: ${name}`);
-    // console.log(`file extension is: ${ext}`);
-    // console.log(`file path is: ${path}`);
-    // console.log(`file content is: ${file}`);
-    console.log(`extracted data is: ${data}`);
-
+  // console.log(`file name is: ${name}`);
+  // console.log(`file extension is: ${ext}`);
+  // console.log(`file path is: ${path}`);
+  // console.log(`file content is: ${file}`);
+  console.log(`extracted data is: ${data}`);
+  var text = JSON.stringify(data) + '\r\n';
+  FS.appendFile("alldata.txt", text, function (err) {
+    if (err) return console.log(err);
+    console.log('successfully appended "' + text + '"');
   });
+
+});
